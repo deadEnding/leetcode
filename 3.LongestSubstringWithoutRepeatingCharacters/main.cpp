6dc111c31f5843ce1068b9005a2c95be7d1fdac3
deadEnding <deadend.endif@gmail.com>
2016-06-03 15:59:17 +0800

    3. Longest Substring Without Repeating Characters done

---
+++ 3.LongestSubstringWithoutRepeatingCharacters/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        const int MAX_CHARS = 256; // 字符总个数
+        int charIndex[MAX_CHARS];  // 记录字符最近一次出现的下标
+        int maxLen = 0;            // 最大长度
+        int curLen = 0;            // 当前长度
+
+        memset(charIndex, -1, sizeof(charIndex));   // 初始化数组
+
+        for (string::size_type ix = 0; ix != s.size(); ++ix) {
+            if (charIndex[s[ix]] == -1 || charIndex[s[ix]] < ix - curLen) {
+                curLen++;
+            } else {
+                curLen = ix - charIndex[s[ix]];
+            }
+            maxLen = curLen > maxLen ? curLen : maxLen;
+            charIndex[s[ix]] = ix;
+        }
+
+        return maxLen;
+    }
+};
+
+
+int main() {
+    string s = "pwwkew";
+
+    Solution solu;
+    cout << solu.lengthOfLongestSubstring(s) << endl;
+
+    return 0;
+}
