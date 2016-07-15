96b9dd4681898cb8ff4014f17d384f6933dc4384
deadEnding <deadend.endif@gmail.com>
2016-07-15 21:04:27 +0800

    modify format.py

--- format.py
+++ format.py
@@ -4,5 +4,9 @@
 import sys
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "Usage: format.py <string>"
+        exit(0)
+
     s = sys.argv[1]
     print ''.join([i.capitalize() for i in s.split(' ')])
