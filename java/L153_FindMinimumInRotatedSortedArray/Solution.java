62400708469685567fe5783fe9d26a830cd8ff2d
deadEnding <deadend.endif@gmail.com>
2016-07-19 21:09:52 +0800

    153. Find Minimum in Rotated Sorted Array done

---
+++ java/L153_FindMinimumInRotatedSortedArray/Solution.java
@@ -0,0 +1,30 @@
+package L153_FindMinimumInRotatedSortedArray;
+
+/**
+ * @author: deadend
+ * @date: 8:27 PM 7/19/16
+ * @version: 1.0
+ * @description: Leetcode 153. Find Minimum in Rotated Sorted Array
+ */
+
+
+public class Solution {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[r])
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return nums[l];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3};
+        System.out.println(new Solution().findMin(nums));
+    }
+}
