19f91aa13a7ca96355f437c248b9e139fcdc99ab
deadEnding <deadend.endif@gmail.com>
2016-07-20 14:03:44 +0800

    169. Majority Element done

---
+++ java/L169_MajorityElement/Solution.java
@@ -0,0 +1,53 @@
+package L169_MajorityElement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: deadend
+ * @date: 1:04 PM 7/20/16
+ * @version: 1.0
+ * @description: Leetcode 169. Majority Element
+ */
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int n : nums) {
+            if (count == 0) {
+                candidate = n;
+                count++;
+            } else {
+                count = (n == candidate ? count + 1 : count - 1);
+            }
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,1};
+        System.out.println(new Solution().majorityElement(nums));
+    }
+}
+
+
+/* 时间复杂度O(n)，空间复杂度O(n) */
+class OtherSolution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : nums) {
+            if (!map.containsKey(i)) {
+                map.put(i, 0);
+            }
+
+            int count = map.get(i) + 1;
+            if (count > nums.length / 2)
+                return i;
+            map.put(i, count);
+        }
+        return -1;
+    }
+}
