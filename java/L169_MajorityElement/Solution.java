2386a57c2f25b392ff36e2d56ec0c94853cf0082
deadEnding <deadend.endif@gmail.com>
2016-07-21 09:46:31 +0800

    169. Majority Element modified

--- java/L169_MajorityElement/Solution.java
+++ java/L169_MajorityElement/Solution.java
@@ -10,6 +10,7 @@ import java.util.Map;
  * @description: Leetcode 169. Majority Element
  */
 
+/* Moore's voting algorithm 时间复杂度O(n)，空间复杂度O(1) */
 public class Solution {
     public int majorityElement(int[] nums) {
         int candidate = 0;
@@ -28,13 +29,31 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] nums = {1,2,1,3,1};
-        System.out.println(new Solution().majorityElement(nums));
+        System.out.println(new BitSolution().majorityElement(nums));
     }
 }
 
 
-/* 时间复杂度O(n)，空间复杂度O(n) */
-class OtherSolution {
+/* 位操作 时间复杂度O(n)，空间复杂度O(1) */
+class BitSolution {
+    public int majorityElement(int[] nums) {
+        final int size = 32;
+        int majority = 0;
+
+        for (int i = 0; i < size; i++) {
+            int bitCounter = 0;
+            for (int n : nums)
+                bitCounter += ((1 << i & n) != 0 ? 1 : 0);
+            if (bitCounter > nums.length / 2)
+                majority += 1 << i;
+        }
+        return majority;
+    }
+}
+
+
+/* 基本思路 时间复杂度O(n)，空间复杂度O(n) */
+class BasicSolution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
 
